97ba161a8676ad4a7a5f3422293bb3a26fc72c18
brian presta <brian.presta@gmail.comgit commit -m first>
2020-07-30 16:44:21 -0700

    set up html get requests

---
+++ server.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+const db = require('./db/db.json')
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
+const PORT = process.env.PORT || 3001;
+const app = express()
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+app.use('/api',apiRoutes)
+app.use('/',htmlRoutes)
+
+app.listen(PORT,function(){
+    console.log('API server now on port 3001!')
+});
